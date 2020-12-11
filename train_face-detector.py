cb26cd0da55a41f9aa1c4aa878eecb413eaad939
alexcamargoweb <alexcamargoweb@gmail.com>
2020-12-11 16:43:07 -0300

    cabecalho dos arquivos

---
+++ train_face-detector.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Face detection - https://github.com/alexcamargoweb/face-detection
+# Detecção de faces com OpenCV e Deep Learning.
+# Adrian Rosebrock, Face detection with OpenCV and deep learning. PyImageSearch.
+# Disponível em: https://www.pyimagesearch.com/2018/02/26/face-detection-with-opencv-and-deep-learning/.
+# Acessado em: 10/12/2020. 
+# Arquivo: train_face-detector.py
+# Execução via Spyder/Linux
+
